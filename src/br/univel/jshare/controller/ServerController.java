d1eaec32ce0f586cce3350d6bba3dc213d1c0150
Matheus Vieira <matheusvieirasto@gmail.com>
2017-04-01 12:53:39 -0300

    Created ServerController

--- src/br/univel/jshare/controller/ServerController.java
+++ src/br/univel/jshare/controller/ServerController.java
@@ -1,6 +1,14 @@
 package br.univel.jshare.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,20 +19,47 @@ import br.univel.jshare.comum.TipoFiltro;
 
 public class ServerController implements IServer{
 
+	private IServer server;
+	private Cliente cliente;
+	private Registry registry;
+	private InetAddress address;
+	private Map<Cliente, List<Arquivo>> mapaClientes = new HashMap<>();
+
 	public ServerController() {
 	}
 
 	public void createServer(){
-		
+		cliente = new Cliente();
+		cliente.setNome("SRVIEIRA");
+		try {
+			cliente.setPorta(3000);
+			cliente.setIp(address.getLocalHost().toString());
+			server = (IServer) UnicastRemoteObject.exportObject(this, 0);
+			registry = LocateRegistry.createRegistry(3000);
+			registry.rebind(IServer.NOME_SERVICO, server);		
+			registrarCliente(cliente);
+		} catch (RemoteException | UnknownHostException e) {
+			e.printStackTrace();
+		} 
 	}
 	
 	public void closeServer(){
-		
+		try {
+			UnicastRemoteObject.unexportObject(this, true);
+			UnicastRemoteObject.unexportObject(registry, true);
+		} catch (NoSuchObjectException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Map<Cliente, List<Arquivo>> getMapaClientes() {
+		return mapaClientes;
 	}
 	
 	@Override
 	public void registrarCliente(Cliente c) throws RemoteException {
-		
+		List<Arquivo> list = new ArrayList<>();
+		mapaClientes.put(c, list);
 	}
 
 	@Override
