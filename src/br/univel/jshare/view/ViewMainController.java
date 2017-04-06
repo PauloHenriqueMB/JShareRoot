09358cc65d7ad8c3869675d3f170b345d8ce0f95
Matheus Vieira <matheusvieirasto@gmail.com>
2017-04-06 20:45:17 -0300

    Update in ViewMain and ViewMainController

--- src/br/univel/jshare/view/ViewMainController.java
+++ src/br/univel/jshare/view/ViewMainController.java
@@ -12,15 +12,20 @@ import java.util.Map;
 import br.univel.jshare.MainApp;
 import br.univel.jshare.comum.Arquivo;
 import br.univel.jshare.comum.Cliente;
+import br.univel.jshare.comum.TipoFiltro;
 import br.univel.jshare.controller.GenerateDialogController;
 import br.univel.jshare.controller.ServerController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ViewMainController{
 
@@ -40,20 +45,33 @@ public class ViewMainController{
 	@FXML
 	private Button btnLimpar;
 	@FXML
-	private ComboBox<?> filtro;
+	private ComboBox<TipoFiltro> filtro;
 	@FXML
 	private TextArea logServer;
+	@FXML
+	private TableView<Arquivo> tableFiles;
+	@FXML
+	private TableColumn<Cliente, String> colAutor = new TableColumn<>("autor");
+	@FXML
+	private TableColumn<Arquivo,String> colArquivo = new TableColumn<>("arquivo");
+	@FXML
+	private TableColumn<Arquivo, String> colExtensao = new TableColumn<>("extensao");
+	@FXML
+	private TableColumn<Arquivo, Date> colData = new TableColumn<>("data");
+	@FXML
+	private TableColumn<Arquivo, Long> colTamanho = new TableColumn<>("tamanho");
+	@FXML
+	private TextField valorFiltro;
 	
 	private Date date;
-	private Stage stage;
 	private boolean clientStatus = false;
 	private boolean serverStatus = false;
-	private Map<Cliente, List<Arquivo>> mapaClientes = new HashMap<>();
 	private ServerController serverController = new ServerController();
 	private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
+	private ObservableMap<Cliente,List<Arquivo>> mapaObserver = FXCollections.observableHashMap();
+	
 	//Client data
-	private InetAddress address;
 	private Cliente client = new Cliente();
 	
 	public ViewMainController() {
@@ -61,8 +79,16 @@ public class ViewMainController{
 	
 	@FXML
 	public void initialize(){
-		disableButtons(true);
+		
 		loadClientData();
+		disableButtons(true);
+		
+		filtro.getItems().add(TipoFiltro.EXTENSAO);
+		filtro.getItems().add(TipoFiltro.TAMANHO_MAX);
+		filtro.getItems().add(TipoFiltro.TAMANHO_MIN);
+		
+		colArquivo.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		
 	}
 	
 	public void loadClientData(){
@@ -85,6 +111,7 @@ public class ViewMainController{
 		btnBuscar.setDisable(status);
 		fieldAssunto.setDisable(status);
 		filtro.setDisable(status);
+		valorFiltro.setDisable(status);
 	}
 	
 	/**
@@ -138,14 +165,41 @@ public class ViewMainController{
 	@FXML
 	public void makeSearch(){
 		Map<Cliente, List<Arquivo>> newMap = new HashMap<>();
-		newMap = serverController.getArchives();
-		newMap.forEach((k,v)->{
+		newMap = serverController.getArchives(fieldAssunto.getText(),filtro.getValue(),valorFiltro.getText());
+		loadDataSearch(newMap);
+	}
+	
+	public void loadDataSearch(Map<Cliente, List<Arquivo>> mapa){
+		
+		mapa.forEach((k,v)->{
 			v.forEach(e->{
-				logServer.appendText(k.getNome()+" possui o arquivo "+e.getNome()+"\n");
+				logServer.appendText(k.getNome()+" tem o arquivo "+e.getNome()+"\n");
+				tableFiles.getColumns().get(0).setText("teste");
 			});
 		});
+//		
+//		List<Arquivo> listaTeste = new ArrayList<>();
+//		
+//		
+//		Arquivo arquivo = new Arquivo();
+//		arquivo.setId(0);
+//		arquivo.setPath("meu path");
+//		arquivo.setNome("teste");
+//		arquivo.setMd5("meu md5");
+//		arquivo.setDataHoraModificacao(new Date());
+//		listaTeste.add(arquivo);
+//		
+//		for (Arquivo a: listaTeste) {
+//            observableList.add(a);
+//        }
+//		
+//		tableFiles.setItems(observableList);
+		
 	}
+	
+	
 
+	
 	public void setUserConnected(String nome){
 		logServer.appendText(nome+" conectou e enviou/atualizou sua lista de arquivo!\n");
 	}
@@ -154,6 +208,7 @@ public class ViewMainController{
 		logServer.appendText(nome+" disconectou do servidor!\n");
 	}
 
+	
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
