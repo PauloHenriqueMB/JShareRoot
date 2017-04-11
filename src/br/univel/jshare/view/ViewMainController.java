f5e6ebc07d0fee1606628ecc721be75ab8d59d2e
Matheus Vieira <matheusvieirasto@gmail.com>
2017-04-11 13:20:55 -0300

    Update in ViewMainController

--- src/br/univel/jshare/view/ViewMainController.java
+++ src/br/univel/jshare/view/ViewMainController.java
@@ -1,37 +1,40 @@
 package br.univel.jshare.view;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import com.sun.xml.internal.bind.v2.runtime.property.Property;
 
 import br.univel.jshare.MainApp;
 import br.univel.jshare.comum.Arquivo;
 import br.univel.jshare.comum.Cliente;
+import br.univel.jshare.comum.IServer;
 import br.univel.jshare.comum.TipoFiltro;
 import br.univel.jshare.controller.GenerateDialogController;
 import br.univel.jshare.controller.ServerController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.ObservableMap;
-import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
 
 public class ViewMainController{
 
@@ -55,32 +58,26 @@ public class ViewMainController{
 	@FXML
 	private TextArea logServer;
 	@FXML
-	private TableColumn<Cliente, String> colAutor = new TableColumn<>("autor");
-	@FXML
-	private TableColumn<Arquivo,String> colArquivo = new TableColumn<>("arquivo");
-	@FXML
-	private TableColumn<Arquivo, String> colExtensao = new TableColumn<>("extensao");
-	@FXML
-	private TableColumn<Arquivo, String> colData = new TableColumn<>("data");
-	@FXML
-	private TableColumn<Arquivo, Long> colTamanho = new TableColumn<>("tamanho");
-	@FXML
 	private TextField valorFiltro;
+	@FXML
+	private StackPane painel;
+	@FXML
+	private Button realizarDownload;
 	
+	private TreeView<String> arvore;
+	private TreeItem<String> raiz;
+	private TreeItem<String> pai;
+    private TreeItem<String> filho;
+    
 	private Date date;
 	private boolean clientStatus = false;
 	private boolean serverStatus = false;
+	private Map<Cliente, List<Arquivo>> newMap = new HashMap<>();
 	private ServerController serverController = new ServerController();
 	private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-	
-	private Map<Cliente, List<Arquivo>> mapaArquivos = new HashMap<>();
-	private ObservableList<Map.Entry<Cliente, List<Arquivo>>> items = FXCollections.observableArrayList(mapaArquivos.entrySet());
-	
-	@FXML
-	private TableView<Map.Entry<Cliente, List<Arquivo>>> tableFiles = new TableView<>(items);
-	
+
 	//Client data
-	private Cliente client = new Cliente();
+	private Cliente cliente = new Cliente();
 	
 	public ViewMainController() {
 	}
@@ -91,24 +88,20 @@ public class ViewMainController{
 		loadClientData();
 		disableButtons(true);
 		
+		filtro.getItems().add(TipoFiltro.NOME);
 		filtro.getItems().add(TipoFiltro.EXTENSAO);
 		filtro.getItems().add(TipoFiltro.TAMANHO_MAX);
 		filtro.getItems().add(TipoFiltro.TAMANHO_MIN);
-		
-		colArquivo.setCellValueFactory(new PropertyValueFactory<>("nome"));
-		colExtensao.setCellValueFactory(new PropertyValueFactory<>("extensao"));
-		
-		colData.setCellValueFactory(new PropertyValueFactory<>("dataHoraModificacao"));
-		colTamanho.setCellValueFactory(new PropertyValueFactory<>("tamanho"));
+		filtro.getSelectionModel().select(0);
 		
 	}
 	
 	public void loadClientData(){
-		client.setId(0);
-		client.setPorta(3000);
-		client.setNome("SRVIEIRA");	
+		cliente.setId(0);
+		cliente.setPorta(3000);
+		cliente.setNome("SRVIEIRA");	
 		try {
-			client.setIp(InetAddress.getLocalHost().getHostAddress());
+			cliente.setIp(InetAddress.getLocalHost().getHostAddress());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
@@ -136,9 +129,9 @@ public class ViewMainController{
 
 		if(!serverStatus){
 			serverStatus = true;
-			serverController.createServer(client,this);
+			serverController.createServer(cliente,this);
 			handleServer.setText("Desligar");
-			System.out.println(client.getIp());
+			System.out.println(cliente.getIp());
 			logServer.appendText("Servidor iniciado "+dateFormat.format(date)+"\n");
 		}else{
 			System.exit(0);
@@ -158,11 +151,11 @@ public class ViewMainController{
 		if(fieldIp.getText().length() >= 6 && fieldPort.getLength() == 4){
 			if(!clientStatus){
 				handleCon.setText("Desconectar");
-				serverController.conectarCliente(client, ip, porta);
+				serverController.conectarCliente(cliente, ip, porta);
 				clientStatus = true;
 				disableButtons(false);
 			}else{
-				serverController.desconectarCliente(client);
+				serverController.desconectarCliente(cliente);
 				handleCon.setText("Conectar");
 				clientStatus = false;
 				disableButtons(true);
@@ -176,39 +169,71 @@ public class ViewMainController{
 	
 	@FXML
 	public void makeSearch(){
-		Map<Cliente, List<Arquivo>> newMap = new HashMap<>();
 		newMap = serverController.getArchives(fieldAssunto.getText(),filtro.getValue(),valorFiltro.getText());
 		loadDataSearch(newMap);
 	}
 	
 	public void loadDataSearch(Map<Cliente, List<Arquivo>> mapa){
 		
-		items.clear();
-		
+		raiz = new TreeItem<>("Arquivos");
 		mapa.forEach((k,v)->{
-			List<Arquivo> listaResultado = new ArrayList<>();
+			pai = new TreeItem<>(k.getNome());
 			v.forEach(e->{
-				logServer.appendText(k.getNome()+" tem o arquivo "+e.getNome()+"\n");
-				listaResultado.add(e);
+				filho = new TreeItem<>(e.getNome());
+				pai.getChildren().add(filho);
 			});
-			mapaArquivos.put(k, listaResultado);
-			
+			raiz.getChildren().add(pai);
 		});
+		arvore = new TreeView<>(raiz);
+		arvore.getEditingItem();
+		painel.getChildren().add(arvore);
 
+	}
+	
+	@FXML
+	public void realizarDownload(){
+		String nome = String.valueOf(arvore.getSelectionModel().getSelectedItem().getParent().getValue());
+		String arquivo = String.valueOf(arvore.getSelectionModel().getSelectedItem().getValue());
 		
-		tableFiles.getColumns().addAll();
-		
-	}
-	
-	
-	public void setUserConnected(String nome){
-		logServer.appendText(nome+" conectou e enviou/atualizou sua lista de arquivo!\n");
-	}
-	
-	public void setUserDisconnected(String nome){
-		logServer.appendText(nome+" disconectou do servidor!\n");
-	}
+		newMap.forEach((k,v)->{
+			if (k.getNome().equals(nome)) {
+			
+				v.forEach(e->{
+					if(e.getNome().equals(arquivo)){
+						try {
+							
+							Registry registry = LocateRegistry.getRegistry(k.getIp(), k.getPorta());
+							IServer service = (IServer) registry.lookup(IServer.NOME_SERVICO);
+							byte[] bytes = service.baixarArquivo(cliente, e);
 
+							String fileName = nome.replace(" ", "").toLowerCase() +  ".copy." + arquivo;
+								
+							File f = new File(fileName);
+							String filePath = "." + File.separatorChar + "share" + File.separatorChar + f;
+							
+							Path path = Files.write(Paths.get(filePath), bytes, StandardOpenOption.CREATE);
+							
+						} catch (RemoteException e1) {
+							e1.printStackTrace();
+						} catch (NotBoundException e1) {
+							e1.printStackTrace();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						
+					}
+				});
+				
+			}
+		});
+	}
+	
+	@FXML
+	public void limparBusca(){
+		painel.getChildren().clear();
+		fieldAssunto.clear();
+		valorFiltro.clear();
+	}
 	
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
